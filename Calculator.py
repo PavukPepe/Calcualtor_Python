538b3f0dbe4751d929b07b922c34c1a98c722f47
P4VUK <96348138+PavukPepe@users.noreply.github.com>
2023-11-10 14:59:53 +0300

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,21 +1,36 @@
 import math
 import time
 
-
-while True:
+def main():
     l = ['1. Сложить 2 числа', '2. Вычесть второе из первого', '3. Перемножить два числа',
          '4. Разделить первое на второе', '5. Возвести в степень N первое число', '6. Найти квадратный корень из числа',
-         '7. Найти факториал', '8. Найти синус', '9. Найти косинус', '10. Найти тангенс', '11. ВЫХОД']
+         '7. Найти факториал', '8. Найти синус', '9. Найти косинус', '10. Найти тангенс', '11. Плозадь треугольника', '12. Проверка на четность', '13. Сумма цифр числа', '14. ВЫХОД']
     for j in l:
         print(j)
+
+def rectangle_area(weight, height):
+    return (math.sqrt(weight**2 + height**2))
+
+def is_even(number):
+    return number%2 == 0
+
+def sum_digits(numbers):
+    sum = 0
+    while numbers > 0:
+        sum += numbers % 10
+        numbers = numbers // 10
+    return sum
+
+while True:
+    main()
     m = input("Введите действие ")
     try:
         m = int(m)
     except:
-        print("Вы ввели некоректное действие")
+        print("Вы ввели некорректное действие")
         continue
-    if m > 12 or m < 1:
-        print("Вы ввели некоректное действие")
+    if m > 14 or m < 1:
+        print("Вы ввели некорректное действие")
     if m == 1 or m == 2 or m == 3 or m == 4:
         try:
             n1 = float(input("Введите первое число "))
@@ -49,7 +64,11 @@ while True:
             print("Вы ввели некорректные данные")
             continue
         if m == 6:
-            print(math.sqrt(n1))
+            try:
+                print(math.sqrt(n1))
+            except:
+                print("Вы ввели некорректные данные")
+                continue
         elif m == 8:
             print(math.sin(math.radians(n1)))
         elif m == 9:
@@ -59,10 +78,34 @@ while True:
     elif m == 7:
         try:
             n1 = int(input("Введите число "))
+            math.factorial(n1)
         except:
             print("Вы ввели некорректные данные")
             continue
         print(math.factorial(n1))
     elif m == 11:
+        try:
+            n1 = int(input("Введите первое число "))
+            n2 = int(input("Введите второе число "))
+        except:
+            print("Вы ввели некорректные данные")
+            continue
+        print(rectangle_area(n1, n2))
+    elif m == 12:
+        try:
+            n1 = int(input("Введите число "))
+        except:
+            print("Вы ввели некорректные данные")
+            continue
+        print(is_even(n1))
+    elif m == 13:
+        try:
+            n1 = int(input("Введите число "))
+        except:
+            print("Вы ввели некорректные данные")
+            continue
+        print(sum_digits(n1))
+    elif m == 14:
         break
     time.sleep(2.5)
+
